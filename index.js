e83b911e18639fe9b4a22ee57691e01b54b810fa
xerxex-odame <kingodame@yahoo.com>
2022-05-13 08:28:37 +0000

    function continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function mondayWork(brandWork="coding-skills"){
+    return `This Monday, I want to ${codingSkills}!`
+  }
+  
+  function tuesdayProject(activity="go to site"){
+    return `This Tuesday, I will ${activity}.`
+  }
+  
+  function wrapAdjective(highlight){
+      return function newfunc(adjective){
+        return `You are ${highlight}${adjective}${highlight}!`
+      }
+  }
